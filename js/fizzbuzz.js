bf0be1f6323d767edcca2bf6791cdf3b17999b85
Joel Ogunniran <joelnirano@gmail.com>
2021-09-02 21:22:13 +0100

    Addin pseudocode for fizzbuzz project
    Adding pseudocode of the fizzbuzz coding project

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,4 +1,48 @@
-// Call Hello World
-function helloWorld(){
-   alert("hello World");
+//1. Get values from the user, we need to get the Fizz and Buzz value.
+function getValues(params) {
+   //Get user values from the DOM
+
+
+   // Parse for the numbers i.e. convert string to integer
+
+
+   //Check the numbers are integer
+
+
+   // We Call FizzBuzz
+
+
+   //Call display data and write the value to the screen
+
+}
+
+//2. 
+
+function fizzBuzz(fizzValue, buzzValue) {
+   //We need to initialize an array
+
+
+   //Loop from 1 to 100
+
+
+   //We check the current value (number) in 4 steps
+   //Check 1: Check if divisible by both 3 & 5 (Fizz and Buzz value), if true push "FizzBuzz" into the array not the number
+   
+   //Check 2: Check if divisible by 3 (Fizz value), if true push "Fizz" into an array and not the number
+
+   //Check 3: Check if divisible by 5 (Buzz value), if true push "Buzz" into an array not the number
+
+   // If none of the numbers are divisible by 3 or 5 then push the number into the array
+
+   //Finally Return the array
+   returnArray;
+
+}
+
+function displayData(fbArray) {
+
+   // Loop over the array and create a table row for each item
+
+   //Add all the rows to the table
+   
 }
